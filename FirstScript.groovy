3a702c62fc805b9863ff08a8c0ecfe8366264483
Marco Antonio González <magonzalezg@viewnext.com>
2019-07-14 10:23:06 +0200

    My first groovy script

---
+++ FirstScript.groovy
@@ -0,0 +1,32 @@
+println('Bazinga!')
+
+
+name = 'John'
+println(name)
+
+
+def name = "John"
+def surname = "Doe"
+println(name + " " + surname)
+//print($name + " " + $surname) //---> References outside quotes does not work.
+//print($name + " " + ${surname}) //---> ${X] outside quotes does not work.
+println("$name ${surname}") //---> We can refer variables in an string with the pattern $X or ${X}
+//print('$name ${surname}') ---> All inside single quotes is literally interpreted. It is considered only text.
+name // It doesn't fail, but neither prints any to console.
+
+/*
+---> References outside quotes doesn't work.
+$name
+${surname}
+*/
+
+/*
+def 1_var = 12345 ---> Variable names must start with an undescore or a letter.
+println(1_var)
+*/
+
+def var_1 = 12345
+println(var_1)
+
+var_2 = 54321 //---> In this static scope, this definition way is totally allowed.
+println(var_2)
